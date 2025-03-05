85c1f672a8af78d9b22974210b34bc5100e6ddfe
Humberto Corrêa <humberto.cgomes17@gmail.com>
2025-03-05 19:35:46 -0300

    Update App.java

--- March/01.04/src/App.java
+++ March/01.04/src/App.java
@@ -3,11 +3,11 @@ package src;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
-        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DISEL, 5, 70);
-        Carro suv = new Carro("SUV", TipoCombustivel.GASOLINA, 8, 55);
-        Carro suvFlex = new Carro("SUVFlex", TipoCombustivel.FLEX, 6, 65);
+        Carro basico        = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
+        Carro esportivo     = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
+        Carro utilitario    = new Carro("Utilitario", TipoCombustivel.DISEL, 5, 70);
+        Carro suv           = new Carro("SUV", TipoCombustivel.GASOLINA, 8, 55);
+        Carro suvFlex       = new Carro("SUVFlex", TipoCombustivel.FLEX, 6, 65);
 
         System.out.println("\nAbastencendo carro basico com gasolina");
         basico.abastece(TipoCombustivel.GASOLINA, 55);
@@ -25,6 +25,11 @@ public class App {
         System.out.println("\nViajando com o carro esportivo");
         esportivo.viaja(270);
         System.out.println(esportivo);
+
+        System.out.println(utilitario);
+        System.out.println(suv);
+        System.out.println(suvFlex);
+
 
 
 
