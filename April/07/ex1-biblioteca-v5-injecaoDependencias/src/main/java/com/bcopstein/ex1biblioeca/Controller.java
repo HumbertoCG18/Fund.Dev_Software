541a2378346ab14292e4dc4951dbb1fd82450c14
Humberto Corrêa <humberto.cgomes17@gmail.com>
2025-04-09 18:15:26 -0300

    update

--- April/07/ex1-biblioteca-v5-injecaoDependencias/src/main/java/com/bcopstein/ex1biblioeca/Controller.java
+++ April/07/ex1-biblioteca-v5-injecaoDependencias/src/main/java/com/bcopstein/ex1biblioeca/Controller.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@SuppressWarnings("unused")
 @RestController
 public class Controller {
     private IAcervoRepository livros;
